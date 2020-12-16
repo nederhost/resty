832ae9fb5d06bfd695ae4120bf717f1ce370a294
Sebastiaan Hoogeveen <sebastiaan@hoogeveen.me>
2020-12-16 11:15:16 +0100

    Modified setup script to automatically build sub packages.

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3.6
 
-from distutils.core import setup
+# from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='Resty',
     version='0.9',
     description='Generic REST client library',
     author='Sebastiaan Hoogeveen',
-    packages=['resty'],
+    packages=find_packages(),
     install_requires=[
       're',
       'urllib',
