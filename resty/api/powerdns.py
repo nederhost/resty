cbd0a6847bff71b18b7ff2a2dd2792b9d4ba75fd
Sebastiaan Hoogeveen <sebastiaan@hoogeveen.me>
2022-10-05 21:42:19 +0200

    Added PowerDNS API client.

---
+++ resty/api/powerdns.py
@@ -0,0 +1,42 @@
+"""
+PowerDNS API client.
+
+# Usage
+```
+import resty.api.powerdns
+
+client = resty.api.powerdns.PowerDNSClient(url='http://localhost:8081/api/v1', server='localhost')
+client.set_authorization('s3cr3tap1k3y')
+
+zone = client.api.server.zones('12345').get()
+```
+"""
+
+import resty.client
+import resty.auth
+
+class PowerDNSClient(resty.client.Client):
+    """
+    The PowerDNS API client.
+    """
+
+    def __init__(self, *arg, url, server='localhost', **kwarg):
+        """
+        Arguments in addition to those of the base class:
+        * url -- the URL of the PowerDNS API
+        * server -- the server name to use
+        """
+        if not url.endswith('/'):
+            url = url + '/'
+        url = url + 'servers/' + server
+        
+        resty.client.Client.__init__(
+            self,
+            root=url,
+            auth=resty.auth.headerauth.Authorization(headername='X-API-Key', type=None),
+            *arg,
+            **kwarg
+        )
+
+    def request_hook(self, request):
+        request.headers['Accept'] = 'application/json'
