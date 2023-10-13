562b64160b896a00557cb811035f1f091d1d5dd3
Sebastiaan Hoogeveen <sebastiaan@hoogeveen.me>
2023-10-13 15:17:34 +0200

    Added autoretry.

--- resty/api/dovecot.py
+++ resty/api/dovecot.py
@@ -27,10 +27,12 @@ class DovecotClient(resty.client.Client):
     The Dovecot API client.
     """
 
-    def __init__(self, *arg, url, **kwarg):
+    def __init__(self, *arg, url, autoretry=5, **kwarg):
         """
         Arguments in addition to those of the base class:
         * url -- the URL of the Dovecot API
+        * autoretry -- number of times to automatically retry an operation
+          if a temporary error occurs, set to 0 to not do any retries
         """
         if not url.endswith('/'):
             url = url + '/'
@@ -43,6 +45,7 @@ class DovecotClient(resty.client.Client):
             *arg,
             **kwarg
         )
+        self.autoretry = autoretry
         self.doveadm = Doveadm(self)
 
     def set_authorization(self, api_key):
@@ -63,14 +66,21 @@ class Doveadm:
         
     def __getattr__(self, name):
         def command(_tag=None, **arguments):
-            type, response, tag = self._client.api.post([name, arguments, _tag if _tag else name]).content[0]
-            if type == 'error':
-                raise resty.exception.ClientError(
-                  client=self._client,
-                  request=[name, arguments, _tag],
-                  response=response,
-                  msg=f"{response.get('exitCode', '')} {response.get('type', '')}",
-                  status=response.get('exitCode', 0)
-                )
-            return response
+            attempt = 0
+            while True:
+                attempt +=1
+                type, response, tag = self._client.api.post([name, arguments, _tag if _tag else name]).content[0]
+                if type == 'error':
+                    exitcode = response.get('exitCode', 0)
+                    if exitcode in (69, 71, 74, 75) and attempt < self._client.autoretry:
+                        continue 
+                    raise resty.exception.ClientError(
+                      client=self._client,
+                      request=[name, arguments, _tag],
+                      response=response,
+                      msg=f"{response.get('exitCode', '')} {response.get('type', '')}",
+                      status=response.get('exitCode', 0)
+                    )
+                else:
+                    return response
         return command
